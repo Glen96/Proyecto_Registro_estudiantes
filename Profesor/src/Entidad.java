a099982099d694d8979bac0a007734cc43a5d633
AdrianSanchezG <90937716+AdrianSanchezG@users.noreply.github.com>
2021-11-07 19:32:19 -0600

    Add files via upload

---
+++ Profesor/src/Entidad.java
@@ -0,0 +1,10 @@
+import java.util.Date;
+
+public abstract class Entidad {
+    public String Nombre;
+    public String Nacionalidad;
+    public short edad;
+    public Date fechaNacimiento;
+    public int cedula;
+    
+}
---
+++ Profesor/src/Profesor.java
@@ -0,0 +1,11 @@
+public class Profesor extends Entidad{
+    public int codProfesor;
+
+    private void Reportes(){
+
+    }
+
+    private void Calificar(){
+
+    }
+}
