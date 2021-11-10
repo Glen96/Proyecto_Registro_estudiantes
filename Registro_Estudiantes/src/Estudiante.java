2eadf734236316208429ef4a986b8ab879c93b6d
AndoreiCR <90808025+AndoreiCR@users.noreply.github.com>
2021-11-09 19:59:47 -0600

    prueba

---
+++ Registro_Estudiantes/src/Estudiante.java
@@ -0,0 +1,10 @@
+import javax.swing.JOptionPane;
+
+public class Estudiante extends Entidad{
+    
+    protected int idEstudiante;
+
+    private void Matricular(){
+        JOptionPane.showMessageDialog(null, "message");
+    }
+}
