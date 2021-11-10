9aab4f01ee7f63105b81be28413bc145b76df921
Glen96 <91099269+Glen96@users.noreply.github.com>
2021-11-09 20:26:53 -0600

    Update Universidad.java

---
+++ Registro_Estudiantes/src/Universidad.java
@@ -0,0 +1,18 @@
+public class Universidad {
+    public String Universidadsede;
+
+    public static Universidad validarmatricula(int cedula){
+        Universidad resultado = null;
+        for (Universidad universidad : estudiante) {
+            if (Universidad.isVisible())
+            
+
+
+
+        }
+
+
+
+    }
+    
+}
