8f70762c29ec45a18b41c21576dd426ad1410d4a
AndoreiCR <dexter_andrey@hotmail.com>
2021-11-09 19:02:36 -0600

    adelanto
    Creacion de los set y gets en entidad

--- Registro_Estudiantes/src/Entidad.java
+++ Registro_Estudiantes/src/Entidad.java
@@ -1,10 +1,50 @@
 import java.util.Date;
 
 public abstract class Entidad {
-    public String Nombre;
-    public String Nacionalidad;
-    public short edad;
-    public Date fechaNacimiento;
-    public int cedula;
-    
+    protected String Nombre;
+    protected String Nacionalidad;
+    protected short edad;
+    protected Date fechaNacimiento;
+    protected int cedula;
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String nombre) {
+        Nombre = nombre;
+    }
+
+    public String getNacionalidad() {
+        return Nacionalidad;
+    }
+
+    public void setNacionalidad(String nacionalidad) {
+        Nacionalidad = nacionalidad;
+    }
+
+    public short getEdad() {
+        return edad;
+    }
+
+    public void setEdad(short edad) {
+        this.edad = edad;
+    }
+
+    public Date getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(Date fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public int getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(int cedula) {
+        this.cedula = cedula;
+    }
+
 }
