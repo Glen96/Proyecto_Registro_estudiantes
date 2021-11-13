1129b77cad7f725815b2019f7113d50e43dc072e
Glen96 <91099269+Glen96@users.noreply.github.com>
2021-11-13 01:52:33 -0600

    Update Profesor.java

--- Registro_Estudiantes/src/Profesor.java
+++ Registro_Estudiantes/src/Profesor.java
@@ -5,13 +5,6 @@ public class Profesor extends Entidad{
     private int notaFinal;
     private String rep;
 
-    public Profesor (String nombre, String Nacionalidad, short edad, Date fechaNaci, int cedula, int notaFinal, String rep){
-        super(nombre, Nacionalidad, edad, fechaNaci, cedula);
-        this.notaFinal=notaFinal;
-        this.rep=rep;
-    }
-
-
     public void Reportes(){
         rep= JOptionPane.showInputDialog(null, "Se hará un reporte al estudiante: "+getNombre()+ "\ncon el # de cedula: "+ getCedula());
         JOptionPane.showMessageDialog(null, "El reporte realizado dice: "+rep);
