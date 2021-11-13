3d7d66cb7272b327bf2fa0ca861ab681baf85d33
Glen96 <91099269+Glen96@users.noreply.github.com>
2021-11-13 01:52:40 -0600

    Update Estudiante.java

--- Registro_Estudiantes/src/Estudiante.java
+++ Registro_Estudiantes/src/Estudiante.java
@@ -3,6 +3,15 @@ import javax.swing.JOptionPane;
 public class Estudiante extends Entidad{
     
     protected int idEstudiante;
+    protected Aula aulaEstudiante;
+
+    public Aula getAulaEstudiante() {
+        return aulaEstudiante;
+    }
+
+    public void setAulaEstudiante(Aula aulaEstudiante) {
+        this.aulaEstudiante = aulaEstudiante;
+    }
 
     private void Matricular(){
         JOptionPane.showMessageDialog(null, "message");
