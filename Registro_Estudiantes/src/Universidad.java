475f670fa43805df3be700f9fa9cb7597761af6f
Glen96 <91099269+Glen96@users.noreply.github.com>
2021-11-13 01:52:30 -0600

    Update Universidad.java

--- Registro_Estudiantes/src/Universidad.java
+++ Registro_Estudiantes/src/Universidad.java
@@ -1,74 +1,28 @@
-public class Universidad {
+import javax.naming.directory.SearchResult;
+import javax.swing.JOptionPane;
+import java.util.ArrayList;
+
+public class Universidad{
+    public matriculaXestudiante matricula = new matriculaXestudiante();
+    
+    
+    public matriculaXestudiante getMatricula() {
+        return matricula;
+    }
 
     public String sede;
-    
-// Opciones de Universidad
-    public public static void opcionesuni() {
 
-        public int option=0;
-        do{
+//asignar sede
+    public void menusede(){
 
-            String arr[]={"1.Elegir sede","2.Consultar matricula","3.Salir"};
-		    cad=JOptionPane.showInputDialog(null,arr,"Universidad",JOptionPane.QUESTION_MESSAGE);
-		    option=Integer.parseInt(cad);
-
-		
-
-               switch (option) {
-                case 1:
-                       this.Universidadsede;
-                       break;
-                case 2:
-                       this.validarmatricula;
-
-               }
-
-        }while (op!=3);
-    
+        sede=JOptionPane.showInputDialog("Indique la sede");
         
-
     }
     
-// Metodo para mostrar sedes de universidad
-    public static void Universidadsede{
-        String VU[] = {
-            "Seleccione la sede",
-            "San Pedro",
-            "Cartago",
-            "Heredia",
-            "Salir",
-            }; 
-            
-            String op = (String) JOptionPane.showInputDialog(null, "Opciones", "Universidad", JOptionPane.DEFAULT_OPTION,VU, VU[0]);
-
-        if ("San Pedro".equals(op))
-        sede="San Pedro";
-
-        if ("Cartago".equals(op))
-        sede="Cartago";
-
-        if ("Heredia".equals(op))
-        sede="Heredia";
-
+    //validar cedula
+    public boolean cedulabuscada;
+    public void validarmatricula(){
     }
+}
 
  
-    //Metodo para busqueda de matricula
-    public static void validarmatricula(int cedula){
-        
-        for (Universidad universidad : Estudiante) {
-            if (Universidad.getCedula().equals(cedula){
-                System.out.println("Estudiante matriculado correctamente");
-            }else {
-                System.out.println("Estudiante no matriculado")
-            }
-
-
-
-        }
-
-
-
-    }
-    
-}
